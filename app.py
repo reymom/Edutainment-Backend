c407396b6084789160b5af12a026f413ec3f47f3
reymom <ramon_gs95@hotmail.com>
2023-12-22 13:20:22 +0100

    cors

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
+import re
 from flask import Flask, jsonify, request
-from api.gemini import generate_questionnaire, generate_story_text
+from flask_cors import CORS
+from api.story_generator import generate_story_text
+from api.question_generator import generate_questionnaire_object
 from api.image_generator import generate_image
 from dotenv import load_dotenv
 import google.generativeai as genai
 import os
 
 load_dotenv()
-app = Flask(__name__)
-
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+app = Flask(__name__)
+CORS(app, resources={r"/generate_story": {"origins": "http://localhost:3000"},
+                     r"/generate_questionnaire": {"origins": "http://localhost:3000"}})
 
 
 @app.route('/generate_story', methods=['POST'])
 def generate_story():
-    image = request.json.get('image')
+    images = request.json.get('images')
     prompt = request.json.get('prompt')
+    width = request.json.get('width')
+    height = request.json.get('height')
+    img_type = request.json.get('format')
 
-    story_text_response = generate_story_text(image, prompt)
+    story_text_response = generate_story_text(
+        prompt, images[0], width, height, img_type)
     return jsonify(story_text_response.__dict__)
 
 
 @app.route('/generate_questionnaire', methods=['POST'])
 def generate_questionnaire():
     story_text = request.json.get('story_text')
-    questionnaire_response = generate_questionnaire(story_text)
+    questionnaire_response = generate_questionnaire_object(story_text)
     return jsonify(questionnaire_response.__dict__)
 
 
